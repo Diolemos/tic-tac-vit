b97407444c91ae60c446aca10a9f16474a984c8f
Dio Lemos <pedro.devsc@outlook.com>
2023-08-06 17:40:05 -0300

    make cross

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import Cell from "./Components/Cell";
 
 const App = () => {
   const [cells, setCells] = useState(["","","","","","","","","",]);
+ const [go,setGo] = useState("circle")
+ const [winningMessage, setWinningMessage] = useState(null) 
  console.log(cells)
   return (
     <div className="app">
       <div className="gameboard">
-      { cells.map((cell,index)=> <Cell key={index} id={index} cell={cell}/>)}
+      { cells.map((cell,index)=> <Cell key={index}
+       id={index} 
+       cell={cell} setCells={setCells}/>)}
         <p></p>
       </div>
     </div>
--- src/Components/Cell.jsx
+++ src/Components/Cell.jsx
@@ -1,13 +1,22 @@
-const Cell = ({id,cell})=>{
+const Cell = ({id,cell, setCells, go})=>{
 
-const handleClick = ()=>{
-    
+const handleClick = (e)=>{
+ const taken = e.target.firstChild.classList.contains("circle") ||
+ e.target.firstChild.classList.contains("cross")
+
+if(!taken){
+
+}
 }
 
 
 
     return(
-        <div className="square" id={id} onClick={handleClick}></div>
+        <div className="square" 
+        id={id} 
+        onClick={handleClick}>
+           
+        </div>
     )
 }
 
