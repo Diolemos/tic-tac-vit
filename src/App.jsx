e229886ca380506338a916f78a397ea1c4542578
Dio Lemos <pedro.devsc@outlook.com>
2023-08-06 18:51:46 -0300

    Fix winning conditions

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,63 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Cell from "./Components/Cell";
 
 const App = () => {
-  const [cells, setCells] = useState(["","","","","","","","","",]);
- const [go,setGo] = useState("circle")
- const [winningMessage, setWinningMessage] = useState(null) 
- const message = " " + go + " turn"
+  const [cells, setCells] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [go, setGo] = useState("circle");
+  const [winningMessage, setWinningMessage] = useState(null);
+  const message = " " + go + " turn";
+  console.log(cells);
+
+  const checkScore = () => {
+    const winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    let circleWins = false;
+    let crossWins = false;
+
+    for (const array of winningCombinations) {
+      circleWins = array.every((cell) => cells[cell] === "circle");
+      crossWins = array.every((cell) => cells[cell] === "cross");
+
+      if (circleWins || crossWins) {
+        break;
+      }
+    }
+
+    if (circleWins) {
+      setWinningMessage("Circle Wins!");
+    } else if (crossWins) {
+      setWinningMessage("Cross Wins!");
+    }
+  };
+
+  useEffect(() => {
+    checkScore();
+  }, [cells]);
+
   return (
     <div className="app">
       <div className="gameboard">
-      { cells.map((cell,index)=> <Cell key={index}
-       id={index} 
-       go={go}
-       setGo={setGo}
-       cells={cells}
-       cell={cell} setCells={setCells}/>)}
-        <p>{message}</p>
+        {cells.map((cell, index) => (
+          <Cell
+            key={index}
+            id={index}
+            go={go}
+            setGo={setGo}
+            cells={cells}
+            cell={cell}
+            setCells={setCells}
+          />
+        ))}
+        <p>{winningMessage || message}</p>
       </div>
     </div>
   );
