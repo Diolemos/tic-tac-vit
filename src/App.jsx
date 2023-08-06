30b88fce0d1fa313627b0f80fc77c8389fef7067
Dio Lemos <pedro.devsc@outlook.com>
2023-08-06 18:02:10 -0300

    update handleclick cell.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,16 @@ const App = () => {
   const [cells, setCells] = useState(["","","","","","","","","",]);
  const [go,setGo] = useState("circle")
  const [winningMessage, setWinningMessage] = useState(null) 
- console.log(cells)
+ const message = " " + go + " turn"
   return (
     <div className="app">
       <div className="gameboard">
       { cells.map((cell,index)=> <Cell key={index}
        id={index} 
+       go={go}
+       setGo={setGo}
        cell={cell} setCells={setCells}/>)}
-        <p></p>
+        <p>{message}</p>
       </div>
     </div>
   );
--- src/Components/Cell.jsx
+++ src/Components/Cell.jsx
@@ -1,11 +1,18 @@
-const Cell = ({id,cell, setCells, go})=>{
+const Cell = ({id,cell, setCells, go, setGo})=>{
 
 const handleClick = (e)=>{
  const taken = e.target.firstChild.classList.contains("circle") ||
  e.target.firstChild.classList.contains("cross")
 
 if(!taken){
-
+    if(go === "circle"){
+        e.target.firstChild.classList.add("circle")
+        setGo("cross")
+    }
+    if(go === "cross"){
+        e.target.firstChild.classList.add("cross")
+        setGo("circle")
+    }
 }
 }
 
@@ -15,7 +22,7 @@ if(!taken){
         <div className="square" 
         id={id} 
         onClick={handleClick}>
-           
+           <div className={cell}></div>
         </div>
     )
 }
